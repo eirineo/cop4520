08a3870e29e0b2480f5659e2f4138315eafd74a9
eirineo <60438888+eirineo@users.noreply.github.com>
2022-01-28 22:50:57 -0500

    test thread constructor

--- HwOne.cpp
+++ HwOne.cpp
@@ -1,23 +1,54 @@
 #include <iostream>
 #include <math.h>
 #include <chrono>
-
+#include <thread>
 #define LIMIT 100000000
+bool isPrime[LIMIT];
 
 using namespace std::chrono;
 //prototypes
-bool* primeSieve();
-void removeMultiples(int index, bool* arr);
+void primeSieve();
+void removeMultiples(int index);
+
+
+class PrimeThread
+{
+    private:
+        int classIndex;
+        bool running;
+    public:
+        PrimeThread()
+        {
+            running = false;
+        }
+        PrimeThread(int i)
+        {
+            classIndex = i;
+            running = false;
+        }//end of constructor
+
+        bool isRunning()
+        {
+            return running;
+        }//end isRunning
+        void run()
+        {
+            std::thread t (removeMultiples, classIndex);
+            t.join();
+            running = false;
+        }//end run
+
+};//end of class
 
 int main()
 {
     auto start = high_resolution_clock::now();
 
-    bool *test = primeSieve();
+    primeSieve();
     int count = 0;
     for(int i = 0; i < LIMIT; i++)
     {
-        if(test[i])
+        if(isPrime[i])
         {
             count++;
         }
@@ -28,30 +59,51 @@ int main()
     auto duration = duration_cast<milliseconds>(stop - start);
     std::cout << duration.count() << "ms" << std::endl;
 }//end main
-bool* primeSieve()
+void primeSieve()
 {
-    bool *isPrime = new bool[LIMIT];
+    //bool *isPrime = new bool[LIMIT];
     std::fill_n(isPrime, LIMIT, true);
+    PrimeThread* first;
+    PrimeThread second;
+    PrimeThread third;
+    PrimeThread fourth;
+    PrimeThread fifth;
+    PrimeThread sixth;
+    PrimeThread seventh;
+    PrimeThread eighth;
+    int temp = 0;
 
     isPrime[0] = false;
     isPrime[1] = false;
+    //this needs to be removed
+    // for(int i = 2; i * i <= LIMIT; i++)
+    // {
+    //     // std::cout << i << std::endl;
+    //     if(isPrime[i])
+    //     {
+    //         removeMultiples(i, isPrime);
+    //     }//end if
+    // }//end for loop
+    //end old stuff
 
-    for(int i = 2; i * i <= LIMIT; i++)
-    {
-        // std::cout << i << std::endl;
-        if(isPrime[i])
+    int index = 0;
+    while(index * index <= LIMIT)
+    {   
+
+        if(isPrime[index])
         {
-            removeMultiples(i, isPrime);
-        }//end if
-    }//end for loop
-    return isPrime;
+            temp = index;
+            index++;            
+            first = new PrimeThread(temp);
+        }
+    }//end while loop
 }//end of primeSieve
 
-void removeMultiples(int index, bool* arr)
+void removeMultiples(int index)
 {
     for(int j = 2 * index; j <= LIMIT; j += index)
     {
-        arr[j] = false;
-    }//end inner for loop
+        isPrime[j] = false;
+    }//end for loop
     return;
 }//end removeMultiples
